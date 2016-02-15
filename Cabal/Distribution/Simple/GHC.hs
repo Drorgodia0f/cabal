Drorgodia0f/cabal

commit 859d6e7f906301ecba08df13051719b186b73af9

Also probe in canonicalized path, if binary was a symlink.
Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

Cabal/Distribution/Simple/GHC.hs
@@ -115,7 +115,8 @@ import Data.Maybe               ( catMaybes )
 import Data.Monoid as Mon       ( Monoid(..) )
 import Data.Version             ( showVersion )
 import System.Directory
-         ( doesFileExist, getAppUserDataDirectory, createDirectoryIfMissing )
+         ( doesFileExist, getAppUserDataDirectory, createDirectoryIfMissing
+         , canonicalizePath )
 import System.FilePath          ( (</>), (<.>), takeExtension,
                                   takeDirectory, replaceExtension,
                                   splitExtension, isRelative )
@@ -200,20 +201,26 @@ guessToolFromGhcPath :: Program -> ConfiguredProgram
                      -> IO (Maybe (FilePath, [FilePath]))
 guessToolFromGhcPath tool ghcProg verbosity searchpath
   = do let toolname          = programName tool
-           path              = programPath ghcProg
-           dir               = takeDirectory path
-           versionSuffix     = takeVersionSuffix (dropExeExtension path)
-           guessNormal       = dir </> toolname <.> exeExtension
-           guessGhcVersioned = dir </> (toolname ++ "-ghc" ++ versionSuffix)
-                               <.> exeExtension
-           guessVersioned    = dir </> (toolname ++ versionSuffix)
-                               <.> exeExtension
-           guesses | null versionSuffix = [guessNormal]
-                   | otherwise          = [guessGhcVersioned,
-                                           guessVersioned,
-                                           guessNormal]
+           given_path        = programPath ghcProg
+           given_dir         = takeDirectory given_path
+       real_path <- canonicalizePath given_path
+       let real_dir           = takeDirectory real_path
+           versionSuffix path = takeVersionSuffix (dropExeExtension path)
+           guessNormal       dir = dir </> toolname <.> exeExtension
+           guessGhcVersioned dir = dir </> (toolname ++ "-ghc" ++ versionSuffix dir)
+                                       <.> exeExtension
+           guessVersioned    dir = dir </> (toolname ++ versionSuffix dir)
+                                       <.> exeExtension
+           mkGuesses dir | null (versionSuffix dir) = [guessNormal dir]
+                         | otherwise                = [guessGhcVersioned dir,
+                                                       guessVersioned dir,
+                                                       guessNormal dir]
+           guesses = mkGuesses given_dir ++
+                            if real_path == given_path
+                                then []
+                                else mkGuesses real_dir
        info verbosity $ "looking for tool " ++ toolname
-         ++ " near compiler in " ++ dir
+         ++ " near compiler in " ++ given_dir
        exists <- mapM doesFileExist guesses
        case [ file | (file, True) <- zip guesses exists ] of
                    -- If we can't find it near ghc, fall back to the usual

commit 4449e145ccb48d9c12e345bbab83c4e6b9b8baa3

Fix bug #3150, regression introduced by 859d6e7.
Added tests for this case, as well as some more.  This
probably breaks the Windows build.

Signed-off-by: Edward Z. Yang <ezyang@cs.stanford.edu>

Cabal/Cabal.cabal
@@ -89,6 +89,24 @@ extra-source-files:
   tests/PackageTests/DuplicateModuleName/tests/Foo.hs
   tests/PackageTests/DuplicateModuleName/tests2/Foo.hs
   tests/PackageTests/EmptyLib/empty/empty.cabal
+  tests/PackageTests/GhcPkgGuess/SameDirectory/SameDirectory.cabal
+  tests/PackageTests/GhcPkgGuess/SameDirectory/ghc
+  tests/PackageTests/GhcPkgGuess/SameDirectory/ghc-pkg
+  tests/PackageTests/GhcPkgGuess/SameDirectoryGhcVersion/SameDirectory.cabal
+  tests/PackageTests/GhcPkgGuess/SameDirectoryGhcVersion/ghc-7.10
+  tests/PackageTests/GhcPkgGuess/SameDirectoryGhcVersion/ghc-pkg-ghc-7.10
+  tests/PackageTests/GhcPkgGuess/SameDirectoryVersion/SameDirectory.cabal
+  tests/PackageTests/GhcPkgGuess/SameDirectoryVersion/ghc-7.10
+  tests/PackageTests/GhcPkgGuess/SameDirectoryVersion/ghc-pkg-7.10
+  tests/PackageTests/GhcPkgGuess/Symlink/SameDirectory.cabal
+  tests/PackageTests/GhcPkgGuess/Symlink/bin/ghc
+  tests/PackageTests/GhcPkgGuess/Symlink/bin/ghc-pkg
+  tests/PackageTests/GhcPkgGuess/SymlinkGhcVersion/SameDirectory.cabal
+  tests/PackageTests/GhcPkgGuess/SymlinkGhcVersion/bin/ghc-7.10
+  tests/PackageTests/GhcPkgGuess/SymlinkGhcVersion/bin/ghc-pkg-7.10
+  tests/PackageTests/GhcPkgGuess/SymlinkVersion/SameDirectory.cabal
+  tests/PackageTests/GhcPkgGuess/SymlinkVersion/bin/ghc-7.10
+  tests/PackageTests/GhcPkgGuess/SymlinkVersion/bin/ghc-pkg-ghc-7.10
   tests/PackageTests/Haddock/CPP.hs
   tests/PackageTests/Haddock/Literate.lhs
   tests/PackageTests/Haddock/NoCPP.hs

Cabal/Distribution/Simple/GHC.hs
@@ -186,22 +186,24 @@ guessToolFromGhcPath tool ghcProg verbosity searchpath
        real_path <- canonicalizePath given_path
        let real_dir           = takeDirectory real_path
            versionSuffix path = takeVersionSuffix (dropExeExtension path)
+           given_suf = versionSuffix given_path
+           real_suf  = versionSuffix real_path
            guessNormal       dir = dir </> toolname <.> exeExtension
-           guessGhcVersioned dir = dir </> (toolname ++ "-ghc"
-                                            ++ versionSuffix dir)
-                                       <.> exeExtension
-           guessVersioned    dir = dir </> (toolname ++ versionSuffix dir)
-                                       <.> exeExtension
-           mkGuesses dir | null (versionSuffix dir) = [guessNormal dir]
-                         | otherwise                = [guessGhcVersioned dir,
-                                                       guessVersioned dir,
-                                                       guessNormal dir]
-           guesses = mkGuesses given_dir ++
+           guessGhcVersioned dir suf = dir </> (toolname ++ "-ghc" ++ suf)
+                                           <.> exeExtension
+           guessVersioned    dir suf = dir </> (toolname ++ suf)
+                                           <.> exeExtension
+           mkGuesses dir suf | null suf  = [guessNormal dir]
+                             | otherwise = [guessGhcVersioned dir suf,
+                                            guessVersioned dir suf,
+                                            guessNormal dir]
+           guesses = mkGuesses given_dir given_suf ++
                             if real_path == given_path
                                 then []
-                                else mkGuesses real_dir
+                                else mkGuesses real_dir real_suf
        info verbosity $ "looking for tool " ++ toolname
          ++ " near compiler in " ++ given_dir
+       debug verbosity $ "candidate locations: " ++ show guesses
        exists <- mapM doesFileExist guesses
        case [ file | (file, True) <- zip guesses exists ] of
                    -- If we can't find it near ghc, fall back to the usual

Cabal/misc/gen-extra-source-files.sh
@@ -9,7 +9,7 @@ fi
 set -ex
 
 find tests -type f \( -name '*.hs' -or -name '*.lhs' -or -name '*.c' -or -name '*.sh' \
-    -or -name '*.cabal' -or -name '*.hsc' -or -name '*.err' -or -name '*.out' \) -and -not -regex ".*/dist/.*" \
+    -or -name '*.cabal' -or -name '*.hsc' -or -name '*.err' -or -name '*.out' -or -name "ghc*" \) -and -not -regex ".*/dist/.*" \
     | awk '/Check.hs$|UnitTests|PackageTester|autogen|PackageTests.hs|IntegrationTests.hs|CreatePipe|^tests\/Test/ { next } { print }' \
     | LC_ALL=C sort \
     | sed -e 's/^/  /' \

Cabal/tests/PackageTests/GhcPkgGuess/SameDirectory/SameDirectory.cabal
@@ -1,0 +1,11 @@
+name:                SameDirectory
+version:             0.1.0.0
+license:             BSD3
+author:              Edward Z. Yang
+maintainer:          ezyang@cs.stanford.edu
+build-type:          Simple
+cabal-version:       >=1.10
+
+library
+  build-depends:       base
+  default-language:    Haskell2010

Cabal/tests/PackageTests/GhcPkgGuess/SameDirectory/ghc
@@ -1,0 +1,6 @@
+#!/bin/sh
+if [ -z "$WITH_GHC" ]; then
+    echo "Need to set WITH_GHC"
+    exit 1
+fi
+exec "$WITH_GHC" "$@"

Cabal/tests/PackageTests/GhcPkgGuess/SameDirectory/ghc-pkg
@@ -1,0 +1,3 @@
+#!/bin/sh
+echo "GHC package manager version 9999999"
+exit 0

Cabal/tests/PackageTests/GhcPkgGuess/SameDirectoryGhcVersion/SameDirectory.cabal
@@ -1,0 +1,11 @@
+name:                SameDirectory
+version:             0.1.0.0
+license:             BSD3
+author:              Edward Z. Yang
+maintainer:          ezyang@cs.stanford.edu
+build-type:          Simple
+cabal-version:       >=1.10
+
+library
+  build-depends:       base
+  default-language:    Haskell2010

Cabal/tests/PackageTests/GhcPkgGuess/SameDirectoryGhcVersion/ghc-7.10
@@ -1,0 +1,6 @@
+#!/bin/sh
+if [ -z "$WITH_GHC" ]; then
+    echo "Need to set WITH_GHC"
+    exit 1
+fi
+exec "$WITH_GHC" "$@"

Cabal/tests/PackageTests/GhcPkgGuess/SameDirectoryGhcVersion/ghc-pkg-ghc-7.10
@@ -1,0 +1,3 @@
+#!/bin/sh
+echo "GHC package manager version 9999999"
+exit 0

Cabal/tests/PackageTests/GhcPkgGuess/SameDirectoryVersion/SameDirectory.cabal
@@ -1,0 +1,11 @@
+name:                SameDirectory
+version:             0.1.0.0
+license:             BSD3
+author:              Edward Z. Yang
+maintainer:          ezyang@cs.stanford.edu
+build-type:          Simple
+cabal-version:       >=1.10
+
+library
+  build-depends:       base
+  default-language:    Haskell2010

Cabal/tests/PackageTests/GhcPkgGuess/SameDirectoryVersion/ghc-7.10
@@ -1,0 +1,6 @@
+#!/bin/sh
+if [ -z "$WITH_GHC" ]; then
+    echo "Need to set WITH_GHC"
+    exit 1
+fi
+exec "$WITH_GHC" "$@"

Cabal/tests/PackageTests/GhcPkgGuess/SameDirectoryVersion/ghc-pkg-7.10
@@ -1,0 +1,3 @@
+#!/bin/sh
+echo "GHC package manager version 9999999"
+exit 0

Cabal/tests/PackageTests/GhcPkgGuess/Symlink/SameDirectory.cabal
@@ -1,0 +1,11 @@
+name:                SameDirectory
+version:             0.1.0.0
+license:             BSD3
+author:              Edward Z. Yang
+maintainer:          ezyang@cs.stanford.edu
+build-type:          Simple
+cabal-version:       >=1.10
+
+library
+  build-depends:       base
+  default-language:    Haskell2010

Cabal/tests/PackageTests/GhcPkgGuess/Symlink/bin/ghc
@@ -1,0 +1,6 @@
+#!/bin/sh
+if [ -z "$WITH_GHC" ]; then
+    echo "Need to set WITH_GHC"
+    exit 1
+fi
+exec "$WITH_GHC" "$@"

Cabal/tests/PackageTests/GhcPkgGuess/Symlink/bin/ghc-pkg
@@ -1,0 +1,3 @@
+#!/bin/sh
+echo "GHC package manager version 9999999"
+exit 0

Cabal/tests/PackageTests/GhcPkgGuess/Symlink/ghc
@@ -1,0 +1,1 @@
+bin/ghc

Cabal/tests/PackageTests/GhcPkgGuess/SymlinkGhcVersion/SameDirectory.cabal
@@ -1,0 +1,11 @@
+name:                SameDirectory
+version:             0.1.0.0
+license:             BSD3
+author:              Edward Z. Yang
+maintainer:          ezyang@cs.stanford.edu
+build-type:          Simple
+cabal-version:       >=1.10
+
+library
+  build-depends:       base
+  default-language:    Haskell2010

Cabal/tests/PackageTests/GhcPkgGuess/SymlinkGhcVersion/bin/ghc-7.10
@@ -1,0 +1,6 @@
+#!/bin/sh
+if [ -z "$WITH_GHC" ]; then
+    echo "Need to set WITH_GHC"
+    exit 1
+fi
+exec "$WITH_GHC" "$@"

Cabal/tests/PackageTests/GhcPkgGuess/SymlinkGhcVersion/bin/ghc-pkg-7.10
@@ -1,0 +1,3 @@
+#!/bin/sh
+echo "GHC package manager version 9999999"
+exit 0

Cabal/tests/PackageTests/GhcPkgGuess/SymlinkGhcVersion/ghc
@@ -1,0 +1,1 @@
+bin/ghc-7.10

Cabal/tests/PackageTests/GhcPkgGuess/SymlinkVersion/SameDirectory.cabal
@@ -1,0 +1,11 @@
+name:                SameDirectory
+version:             0.1.0.0
+license:             BSD3
+author:              Edward Z. Yang
+maintainer:          ezyang@cs.stanford.edu
+build-type:          Simple
+cabal-version:       >=1.10
+
+library
+  build-depends:       base
+  default-language:    Haskell2010

Cabal/tests/PackageTests/GhcPkgGuess/SymlinkVersion/bin/ghc-7.10
@@ -1,0 +1,6 @@
+#!/bin/sh
+if [ -z "$WITH_GHC" ]; then
+    echo "Need to set WITH_GHC"
+    exit 1
+fi
+exec "$WITH_GHC" "$@"

Cabal/tests/PackageTests/GhcPkgGuess/SymlinkVersion/bin/ghc-pkg-ghc-7.10
@@ -1,0 +1,3 @@
+#!/bin/sh
+echo "GHC package manager version 9999999"
+exit 0

Cabal/tests/PackageTests/GhcPkgGuess/SymlinkVersion/ghc
@@ -1,0 +1,1 @@
+bin/ghc-7.10

Cabal/tests/PackageTests/PackageTester.hs
@@ -15,6 +15,7 @@ module PackageTests.PackageTester
     , distDir
     , relativeDistDir
     , sharedDBPath
+    , getWithGhcPath
 
     -- * Running cabal commands
     , cabal
@@ -246,6 +247,11 @@ sharedDBPath :: TestM FilePath
 sharedDBPath = do
     top_dir <- topDir
     return $ top_dir </> "packagedb"
+
+getWithGhcPath :: TestM FilePath
+getWithGhcPath = do
+    (suite, _) <- ask
+    return $ withGhcPath suite
 
 ------------------------------------------------------------------------
 -- * Running cabal

Cabal/tests/PackageTests/Tests.hs
@@ -229,8 +229,25 @@ nonSharedLibTests config =
         assertOutputContains "Flags chosen: build-exe=False" r
         cabal "build" []
 
+  , tc "GhcPkgGuess/SameDirectory" $ ghc_pkg_guess "ghc"
+  , tc "GhcPkgGuess/SameDirectoryVersion" $ ghc_pkg_guess "ghc-7.10"
+  , tc "GhcPkgGuess/SameDirectoryGhcVersion" $ ghc_pkg_guess "ghc-7.10"
+
+  -- TODO: Disable these tests on Windows
+  , tc "GhcPkgGuess/Symlink" $ ghc_pkg_guess "ghc"
+  , tc "GhcPkgGuess/SymlinkVersion" $ ghc_pkg_guess "ghc"
+  , tc "GhcPkgGuess/SymlinkGhcVersion" $ ghc_pkg_guess "ghc"
+
   ]
   where
+    ghc_pkg_guess bin_name = do
+        cwd <- packageDir
+        with_ghc <- getWithGhcPath
+        r <- withEnv [("WITH_GHC", Just with_ghc)]
+           . shouldFail $ cabal' "configure" ["-w", cwd </> bin_name]
+        assertOutputContains "is version 9999999" r
+        return ()
+
     -- Shared test function for BuildDeps/InternalLibrary* tests.
     internal_lib_test expect = withPackageDb $ do
         withPackage "to-install" $ cabal_install []