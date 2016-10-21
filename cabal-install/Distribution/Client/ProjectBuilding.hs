Drorgodia0f/cabal

commit 11c2307a2f825ac569777da80103a3fc404033bf

Reduce path lengths for temp package src and build dirs
This is for paths under $dist/tmp/ where we unpack package source
tarballs and build them. This is for packages destined for the store.
The names of these tmp dirs don't matter since they're guaranteed to be
unique, so we only need short identifiers. So stop using full package
names, and put the build artifacts in their own dir, rather than using a
dist dir under the src dir.

cabal-install/Distribution/Client/ProjectBuilding.hs
@@ -743,15 +743,16 @@ withTarballLocalDirectory verbosity distDirLayout@DistDirLayout{..}
                           tarball pkgid dparams buildstyle pkgTextOverride
                           buildPkg  =
       case buildstyle of
-        -- In this case we make a temp dir, unpack the tarball to there and
-        -- build and install it from that temp dir.
+        -- In this case we make a couple temp dirs, unpack the tarball to one
+        -- and build and install it from the other. We avoid nesting the
+        -- builddir under the tarball src dir to keep path name lengths down.
         BuildAndInstall ->
-          withTempDirectory verbosity distTempDirectory
-                            (display (packageName pkgid)) $ \tmpdir -> do
-            unpackPackageTarball verbosity tarball tmpdir
+          let tmpdir = distTempDirectory in
+          withTempDirectory verbosity tmpdir "src"   $ \unpackdir ->
+          withTempDirectory verbosity tmpdir "build" $ \builddir -> do
+            unpackPackageTarball verbosity tarball unpackdir
                                  pkgid pkgTextOverride
-            let srcdir   = tmpdir </> display pkgid
-                builddir = srcdir </> "dist"
+            let srcdir   = unpackdir </> display pkgid
             buildPkg srcdir builddir
 
         -- In this case we make sure the tarball has been unpacked to the

commit 2fca04a40da58237d0b1427c953f997561542c4e

Fix install of packages bundling generated source in dist/ (#4012)
Packages like `alex` include pre-generated lexer/parser source
in their source tarball as e.g.

    dist/build/alex/alex-tmp/Scan.hs
    dist/build/alex/alex-tmp/Parser.hs

to avoid having to have `alex` already installed before building
`alex`...

Unfortunately, 11c2307a2f825ac569777da80103a3fc404033bf broke
packages relying on this accidental feature by changing where the
distdir points to. This patch partly reverts that commit and
therefore addresses the regression aspect of #4009.

This fix was suggested by @dcoutts

cabal-install/Distribution/Client/ProjectBuilding.hs
@@ -744,16 +744,21 @@ withTarballLocalDirectory verbosity distDirLayout@DistDirLayout{..}
                           tarball pkgid dparams buildstyle pkgTextOverride
                           buildPkg  =
       case buildstyle of
-        -- In this case we make a couple temp dirs, unpack the tarball to one
-        -- and build and install it from the other. We avoid nesting the
-        -- builddir under the tarball src dir to keep path name lengths down.
+        -- In this case we make a temp dir (e.g. tmp/src2345/), unpack
+        -- the tarball to it (e.g. tmp/src2345/foo-1.0/), and for
+        -- compatibility we put the dist dir within it
+        -- (i.e. tmp/src2345/foo-1.0/dist/).
+        --
+        -- Unfortunately, a few custom Setup.hs scripts do not respect
+        -- the --builddir flag and always look for it at ./dist/ so
+        -- this way we avoid breaking those packages
         BuildAndInstall ->
           let tmpdir = distTempDirectory in
-          withTempDirectory verbosity tmpdir "src"   $ \unpackdir ->
-          withTempDirectory verbosity tmpdir "build" $ \builddir -> do
+          withTempDirectory verbosity tmpdir "src"   $ \unpackdir -> do
             unpackPackageTarball verbosity tarball unpackdir
                                  pkgid pkgTextOverride
             let srcdir   = unpackdir </> display pkgid
+                builddir = srcdir </> "dist"
             buildPkg srcdir builddir
 
         -- In this case we make sure the tarball has been unpacked to the