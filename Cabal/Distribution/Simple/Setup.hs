Drorgodia0f/cabal

commit bf48f759abaac031649d00b0664fe4857fe12edc

Use parseFlagAssignment to parse flag assignments in cli

Cabal/Distribution/Simple/Setup.hs
@@ -672,7 +672,9 @@ configureOptions showOrParseArgs =
       ,option "f" ["flags"]
          "Force values for the given flags in Cabal conditionals in the .cabal file.  E.g., --flags=\"debug -usebytestrings\" forces the flag \"debug\" to true and \"usebytestrings\" to false."
          configConfigurationsFlags (\v flags -> flags { configConfigurationsFlags = v })
-         (reqArg' "FLAGS" readFlagList showFlagList)
+         (reqArg "FLAGS"
+              (readP_to_E (\err -> "Invalid flag assignment: " ++ err) parseFlagAssignment)
+              (map showFlagValue))
 
       ,option "" ["extra-include-dirs"]
          "A list of directories to search for header files"
@@ -769,15 +771,6 @@ configureOptions showOrParseArgs =
          (boolOpt' ([], ["disable-response-files"]) ([], []))
       ]
   where
-    readFlagList :: String -> FlagAssignment
-    readFlagList = map tagWithValue . words
-      where tagWithValue ('-':fname) = (mkFlagName (lowercase fname), False)
-            tagWithValue fname       = (mkFlagName (lowercase fname), True)
-
-    showFlagList :: FlagAssignment -> [String]
-    showFlagList fs = [ if not set then '-':unFlagName fname else unFlagName fname
-                      | (fname, set) <- fs]
-
     liftInstallDirs =
       liftOption configInstallDirs (\v flags -> flags { configInstallDirs = v })
 

commit fa2ecb08ac2cce77bbc0e5c16c882327368cb140

Fix minor regression in flag assignment marshalling to custom-setup
This was introduced by bf48f759abaac031649d00b0664fe4857fe12edc which
resulted in passing `--flags=+foo` to older custom setup scripts which
wouldn't recognize the `+` prefix.

This was reported by @cocreature

Cabal/Distribution/Simple/Setup.hs
@@ -674,7 +674,7 @@ configureOptions showOrParseArgs =
          configConfigurationsFlags (\v flags -> flags { configConfigurationsFlags = v })
          (reqArg "FLAGS"
               (readP_to_E (\err -> "Invalid flag assignment: " ++ err) parseFlagAssignment)
-              (map showFlagValue))
+              (map showFlagValue'))
 
       ,option "" ["extra-include-dirs"]
          "A list of directories to search for header files"
@@ -777,6 +777,13 @@ configureOptions showOrParseArgs =
     reqPathTemplateArgFlag title _sf _lf d get set =
       reqArgFlag title _sf _lf d
         (fmap fromPathTemplate . get) (set . fmap toPathTemplate)
+
+    -- We can't use 'showFlagValue' because legacy custom-setups don't
+    -- support the '+' prefix in --flags; so we omit the (redundant) + prefix;
+    -- NB: we assume that we never have to set/enable '-'-prefixed flags here.
+    showFlagValue' :: (FlagName, Bool) -> String
+    showFlagValue' (f, True)   =       unFlagName f
+    showFlagValue' (f, False)  = '-' : unFlagName f
 
 readPackageDbList :: String -> [Maybe PackageDB]
 readPackageDbList "clear"  = [Nothing]